489b9dd541e448e20ad1bfd0327664d9ad2b65df
Hello Git <wang.yixuan@live.fr>
2022-08-24 09:37:09 -0400

    SignIn Link

---
+++ src/components/HelloUser/HelloUser.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HelloUser.css";
+
+function HelloUser() {
+  return (
+   
+      <div className="user-name">
+        Hello, <span>User</span>
+      </div>
+
+  );
+}
+export default HelloUser;
--- src/components/SignInLink/SignInLink.jsx
+++ src/components/SignInLink/SignInLink.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FaSignInAlt } from "react-icons/fa";
+/*import { FaSignOutAlt } from "react-icons/fa";*/
 import { Link } from "react-router-dom";
 import "./SignInLink.css";
 
 function SignInLink() {
   return (
-    <div className="login-container">
+    <div className="sign-in-container" >
       <FaSignInAlt />
-      <Link to="login" className="sign-in-link">
+      <Link to="login" className="sign-in-link" >
         Sign In
       </Link>
     </div>
